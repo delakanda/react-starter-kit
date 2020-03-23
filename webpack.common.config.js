45fac7b199f4ed17099277824b47c60ee9b9201e
Delali Kanda <delali@spryonline.com>
2020-03-23 23:11:34 +0000

    setting up package json and webpack

---
+++ webpack.common.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+// import path from 'path';
+
+const config = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+		filename: 'app.bundle.js',
+		chunkFilename: 'vendor.bundle.js'
+    },
+    resolve: {
+		extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.css'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file?name=[name].[ext]'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+				use: [
+					{ 
+                        loader: 'style-loader'
+                    },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file?hash=sha512&digest=hex&name=Content/img/flags.[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
+                    { 
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-react', '@babel/preset-env']
+                        }
+                    },
+                    { loader: 'ts-loader' }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+};
+
+module.exports = config;
